6362ed1366aa327a7052e2c60d7b2a2770b30a8c
Eric Satoshi <satoshi.eric@gmail.com>
2022-12-02 00:30:50 -0300

    Implementando método mágico == para classe Dimension

--- src/curve_utils/dimension.py
+++ src/curve_utils/dimension.py
@@ -3,3 +3,6 @@ from src.curve_utils.pair import Pair
 class Dimension(Pair):
     def __init__(self, x:int, y:int) -> None:
         super().__init__(x, y)
+
+    def __eq__(self, other: 'Dimension') -> bool:
+        return self.x == other.x and self.y == other.y
