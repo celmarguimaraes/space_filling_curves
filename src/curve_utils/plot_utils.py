8dd7e3c07ea0c5b9a33ab222fd6d93a98fae83be
Eric Satoshi <satoshi.eric@gmail.com>
2022-12-01 21:45:38 -0300

    Módulo para plotar os gráficos criado

---
+++ src/curve_utils/plot_utils.py
@@ -0,0 +1,50 @@
+from typing import List
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+from src.curve import Curve
+
+
+def _create_segments(x: List, y: List) -> List[List]:
+    '''
+    Create segments from x and y coordinates.
+    Takes a list of x coordinates and a list of y coordinates and returns a list of segments.
+    Each segment is a list of two points.
+    x = [1, 2, 3, 4]
+    y = [1, 2, 3, 4]
+    segments = [[[1, 1], [2, 2]], [[2, 2], [3, 3]], [[3, 3], [4, 4]]]
+    '''
+    points = list(zip(x, y))
+    segments = list(zip(points[:-1], points[1:]))
+    return segments
+
+
+def lineplot(x: List, y: List, cmap=plt.get_cmap('coolwarm'), linewidth=3, alpha=1.0):
+    '''
+    Create a line plot from x and y coordinates.
+    Takes a list of x coordinates and a list of y coordinates and returns a line plot.
+    '''
+    z = np.linspace(0.0, 1.0, len(x))
+    
+    segments = _create_segments(x, y)
+    lc = LineCollection(segments, array=z, cmap=cmap, linewidth=linewidth, alpha=alpha)
+    
+    ax = plt.gca()
+    ax.add_collection(lc)
+    plt.xlim(min(x) - 0.5, max(x) + 0.5)
+    plt.ylim(min(y) - 0.5, max(y) + 0.5)
+    ax.invert_yaxis()
+    
+    return lc
+
+
+def get_xy_from_curve(num_el, curve: Curve):
+    coords = [(curve.get_coordinate(i).x, curve.get_coordinate(i).y) for i in range(num_el)]
+    x, y = zip(*coords)
+    return x, y
+
+
+def plot_curve(num_el, curve: Curve, cmap=plt.get_cmap('coolwarm'), linewidth=3, alpha=1.0):
+    x, y = get_xy_from_curve(num_el, curve)
+    return lineplot(x, y, cmap, linewidth, alpha)
