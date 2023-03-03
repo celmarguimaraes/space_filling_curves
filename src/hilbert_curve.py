a7ab9b748e8a13ff6a1f9048cf90b2f75a51aba7
Eric Satoshi <satoshi.eric@gmail.com>
2023-03-03 15:36:58 -0300

    Curva de hilbert implementada

---
+++ src/hilbert_curve.py
@@ -0,0 +1,42 @@
+from hilbertcurve.hilbertcurve import HilbertCurve as HC
+from typing import List
+from math import log2
+import math
+
+from src.curve import Curve
+from src.curve_utils.coordinate import Coordinate
+from src.curve_utils.dimension import Dimension
+
+
+class HilbertCurve(Curve):
+    map_d_to_xy: List[Coordinate]
+    map_xy_to_d: List[List[int]]
+    curve_generator: HC
+
+    def __init__(self, num_of_elements: int, dimension: Dimension) -> None:
+        if dimension.x != dimension.y:
+            raise ValueError('A altura e largura devem iguais.')
+        if not self.is_power_of_2(dimension.x) or not self.is_power_of_2(dimension.y):
+            raise ValueError('Altura e largura devem ser potência de 2.')
+        if num_of_elements > dimension.x * dimension.y:
+            raise ValueError('Número de elementos não pode ser maior que a quantidade de coordenadas disponíveis na dimensão.')
+
+        super().__init__(num_of_elements, dimension)
+        self.curve_generator = HC(p=log2(dimension.x), n=2)
+
+
+    def is_power_of_2(self, number: int):
+        return log2(number) - int(log2(number)) == 0
+    
+
+    def get_d(self, coordinate: Coordinate) -> int:
+        return self.curve_generator.distance_from_point([coordinate.x, coordinate.y])
+
+
+    def get_coordinate(self, d: int) -> Coordinate:
+        coord = self.curve_generator.point_from_distance(d)
+        return Coordinate(coord[0], coord[1])
+    
+    def define_dimension(self, number_of_elements:int) -> Dimension:
+       d = math.ceil(math.sqrt(number_of_elements))
+       return Dimension(d, d)
